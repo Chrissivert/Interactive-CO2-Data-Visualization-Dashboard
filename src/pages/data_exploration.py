ee9adde71008c039428b4a36911e627422848490
chris <chrisssy@stud.ntnu.no>
2024-11-14 11:55:05 +0100

    can showcase data in logarithmic scale

--- src/pages/data_exploration.py
+++ src/pages/data_exploration.py
@@ -1,19 +1,35 @@
 import streamlit as st
 import plotly.express as px
-
 import src.service as s
+import numpy as np  
 
 def page(dataframes, selected_countries, selected_year_range):
-  st.title("Data Exploration")
-  
-  tab1, tab2 = st.tabs(["📈 Chart", "📋 Table"])
-  for dataframe in dataframes:
-    tab1.plotly_chart(chart(dataframe, selected_countries, selected_year_range), use_container_width=True)
-    tab2.write(dataframe[dataframe["Entity"].isin(selected_countries)])
-  
-def chart(dataframe, selected_country, selected_year_range):
-  filtered_data = dataframe[(dataframe["Entity"].isin(selected_country)) & (dataframe["Year"] >= selected_year_range[0]) & (dataframe["Year"] <= selected_year_range[1])]
+    st.title("Data Exploration")
     
-  metric_column = s.get_unique_column_names(dataframe)[0]
+    tab1, tab2 = st.tabs(["📈 Chart", "📋 Table"])
     
-  return px.line(filtered_data, x="Year", y=metric_column, color="Entity", title=f"{metric_column} over time")
+    for dataframe in dataframes:
+        with tab1:
+            scale_type = st.radio("Select Y-axis scale", ("Linear", "Logarithmic"))
+            log_scale = scale_type == "Logarithmic"  
+            tab1.plotly_chart(chart(dataframe, selected_countries, selected_year_range, log_scale), use_container_width=True)
+        
+        with tab2:
+            tab2.write(dataframe[dataframe["Entity"].isin(selected_countries)])
+
+def chart(dataframe, selected_country, selected_year_range, log_scale=False):
+    filtered_data = dataframe[(dataframe["Entity"].isin(selected_country)) & 
+                              (dataframe["Year"] >= selected_year_range[0]) & 
+                              (dataframe["Year"] <= selected_year_range[1])]
+    
+    last_column = filtered_data.columns[-1]
+    
+    if log_scale:
+        filtered_data[last_column] = np.log10(filtered_data[last_column])
+
+    return px.line(filtered_data, 
+                   x="Year", 
+                   y=last_column, 
+                   color="Entity", 
+                   title=f"{last_column} over time",
+                   log_y=log_scale) 
