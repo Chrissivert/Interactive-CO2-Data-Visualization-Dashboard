2b825c2fe7ff1e73ec4f617e4719ffb6e2ec2fc9
Vegard Arnesen Mytting <120718174+VegardMytting@users.noreply.github.com>
2024-11-15 10:36:17 +0100

    Refactoring

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import streamlit as st
 
 import src.pages.data_exploration
+import src.pages.home
 import src.service as s
 import src.pages.trend_analysis
+import src.page_names as pn
+import src.pages.sidebar as sidebar
 
 st.set_page_config(
-  page_title="Data Visualization Dashboard",
+  page_title="Interactive Data Visualization Dashboard",
   layout="wide",
   initial_sidebar_state="expanded"
 )
@@ -16,27 +19,13 @@ if uploaded_files:
   dataframes = s.load_csv_data(uploaded_files)
   st.write(":green[Datasets loaded successfully!]" if len(dataframes) > 1 else ":green[Dataset loaded successfully!]")
   
-  page = st.sidebar.selectbox("Select Page", [
-    "Home", "Data Exploration", "Trend Analysis"
-  ])
+  page, selected_country, selected_year_range = sidebar.display(dataframes)
 
-  metrics = {}
-  for dataframe in dataframes:
-    for column in s.get_unique_column_names(dataframe):
-      metrics[column] = (dataframe, column)
-
-  st.sidebar.header("Filter Data")
-  countries = s.get_unique_countries(dataframes)
-  selected_country = st.sidebar.multiselect("Select Country", countries, default=countries[:2])
-
-  years = s.get_unique_years(dataframes)
-  selected_year_range = st.sidebar.slider("Select Year Range", min(years), max(years), (min(years), max(years)))
-
-  if page == "Home":
-    st.title("Welcome to the Data Visualization Dashboard")
+  if page == pn.Pages.HOME.value:
+    src.pages.home.page()
       
-  elif page == "Data Exploration":
+  elif page == pn.Pages.DATA_EXPLORATION.value:
     src.pages.data_exploration.page(dataframes, selected_country, selected_year_range)
 
-  elif page == "Trend Analysis":
-    src.pages.trend_analysis.page(metrics, selected_country, selected_year_range)
+  elif page == pn.Pages.TREND_ANALYSIS.value:
+    src.pages.trend_analysis.page(s.get_metrics(dataframes), selected_country, selected_year_range)
