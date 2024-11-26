24e8fc330c0a430d300c0ef17de7de2357fe7851
chris <chrisssy@stud.ntnu.no>
2024-11-26 09:55:06 +0100

    removed comments in data loading

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import src.pages.sidebar as sidebar
 
 import plotly.graph_objects as go
 
-# Set Streamlit configuration
 st.set_page_config(
     page_title="Interactive Data Visualization Dashboard",
     layout="wide",
@@ -13,23 +12,16 @@ st.set_page_config(
 )
 st.title("Interactive Data Visualization Dashboard")
 
-# Load the default dataset
 default_file_path = "data/owid-co2-data.csv" 
 test = "data/renewable-share-energy.csv"
 test2 = "data/emissions-weighted-carbon-price.csv"
 # try:
 default_dataframe = s.load_default_file(default_file_path)
-#     st.write(":green[Default dataset loaded successfully!]")  # Inform the user that the dataset is loaded
 dataframes = [default_dataframe]
-# except FileNotFoundError:
-#     st.error("Default file not found. Please ensure it exists at the specified path.")
-#     dataframes = []
 
 if dataframes:
-    # Display sidebar for continent, country, and year selection
     selected_continent, selected_country, selected_year_range, target_column = sidebar.display(dataframes)
     
-    # Pass selected continent, selected country, selected year range, and target column to the data exploration page
     src.pages.data_exploration.page(dataframes, selected_continent, selected_country, selected_year_range, target_column)
 else:
     st.warning("No datasets available. Please check the default file path.")
